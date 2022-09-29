6495ac43a6459f0434ca46d18d231cfa27617c3e
“SethMegbetor” <sethmegbetor@gmail.com>
2022-09-29 18:03:00 +0000

    added bmi calculator route

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ app.post('/', (req, res) => {
   res.send(`The result of the calculation is: ${result}`);
 });
 
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmiCalculator', (req, res) => {
+  let weight = Number(req.body.weight);
+  let height = Number(req.body.height);
+
+  let bmi = weight / (height * height);
+  res.send(`<h1>Your BMI is ${bmi}</h1>`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server started. App listening on port ${PORT}`);
 });
